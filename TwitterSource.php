54b01dadbb6a32c8273527d6fd657772e59d6412
Skyler Lewis <sblnog@gmail.com>
2012-08-12 10:45:07 -0600

    BugFix - ::find('all') without conditions caused error, resolved

--- TwitterSource.php
+++ TwitterSource.php
@@ -5,13 +5,36 @@
  * @link http://sixteenink.com
  * @link http://github.com/alairock
  **/
-
 App::uses('HttpSocket', 'Network/Http');
 class TwitterSource extends DataSource {
 
+	protected $_schema = array(
+			'id' => array('type' => 'integer', 'key' => 'primary'),
+			'text' => array('type' => 'string'),
+			'created_at' => array('type' => 'string')
+	);
+
+
+	public function __construct($config) {
+		parent::__construct($config);
+		$this->sourceUrl = $this->config['sourceUrl'];
+		$this->Http = new HttpSocket();
+	}
+
+	public function listSources() {
+		return null;
+	}
+
+	public function describe($Model) {
+		return $this->_schema;
+	}
+
 	public function read($model, $queryData = array()) {
+		
 		//Override Config duing a 'find' with the following merge
+		if(!empty($queryData['conditions'])) {
 		$this->config =	array_merge($this->config, $queryData['conditions']);
+		}
 		//go get tweets
 		$results = json_decode($this->Http->get($this->sourceUrl, $this->config));
 		//Show Only Tweet and TimeStamp
@@ -26,26 +49,6 @@ class TwitterSource extends DataSource {
 			return $results;
 		}
 
-	}
-
-	protected $_schema = array(
-			'id' => array('type' => 'integer', 'key' => 'primary'),
-			'text' => array('type' => 'string'),
-			'created_at' => array('type' => 'string')
-		);
-
-	public function __construct($config) {
-		parent::__construct($config);
-		$this->sourceUrl = $this->config['sourceUrl'];
-		$this->Http = new HttpSocket();
-	}
-
-	public function listSources() {
-		return null;
-	}
-
-	public function describe($Model) {
-		return $this->_schema;
 	}
 
 }
